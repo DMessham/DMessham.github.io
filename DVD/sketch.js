7d762b3d62ed815c126aff1351bfba3690df390b
DMessham <lolcato2700@gmail.com>
2021-09-27 13:12:42 -0600

    fixed DVD framerate and collision issues

--- DVD/sketch.js
+++ DVD/sketch.js
@@ -2,9 +2,8 @@
 
 //TODO: Fix extreme slowdown on color shift
 
-let refresh = 60//target framerate, used to calc realfps
-let runTime = 0, startTime = 0, millisecond = 0;
-
+let refresh = 60;//target framerate, used to calc realfps
+let startTime = 0;
 
 let x0 = 200;
 let y0 = 200; //initial position
@@ -34,15 +33,15 @@ function setup() {
   bgBright=randomGaussian(128,128);
   background(bgHue, bgSat, bgBright);
   strokeWeight(0);
-  fill(168)
+  fill(168);
   colorMode(HSB);
 }
 
 function windowResized(){
   createCanvas(windowWidth, windowHeight);
   background(bgHue, bgSat, bgBright);
-  x0=200;
-  y0= 200;
+  x0=10;
+  y0=10;
 }
 
 function keyPressed(){
@@ -68,37 +67,32 @@ function draw() {
 }
 
 function move(){//basic colllision logic
-    x0 += dx0*speed ;//increase x by dx every time the screen redraws
-  
+
   if (x0 + sizeX0 >= width || x0 <= 0){//change direction if the object is within size of the edge of x
     dx0=-dx0;//invert direction
-  tint(random(128,255),random(128,255),random(128,255))//randomly tint the image
+    x0 += dx0*speed*(deltaTime/((1/refresh)*1000))
+    tint(random(128,255),random(128,255),random(128,255))//randomly tint the image
   }
-    y0 += dy0*speed ;//move in y axis
+  x0 += dx0*speed*(deltaTime/((1/refresh)*1000));//increase x by dx every time the screen redraws
   
   if (y0 + sizeY0 >= height || y0 <= 0){//change direction if the object is within size of the edge of y
     dy0=-dy0;//invert direction
-    tint(random(128,255),random(128,255),random(128,255))//randomly tint the image
+    y0 += dy0*speed*(deltaTime/((1/refresh)*1000))
+    tint(random(128,255),random(128,255),random(128,255));//randomly tint the image
   }
-  if (x0 + sizeY0 + 1 >= mouseX && x0 -1 <= mouseX || y0 + sizeY0+1 >= mouseY && y0 -1 <= mouseY){
-    
-    
-  }
+  y0 += dy0*speed*(deltaTime/((1/refresh)*1000));//move in y axis
 }
 function txtInfo(){
   let infoString = "Daniel Messham's JavaScript DVD Logo Bounce\nComp sci 30, S1, Friday September 17th, 2021";//information string used for author info
   
   let realFPS = round((refresh/deltaTime)*20,2);//calculate frame rate using delta time(aka how long it took to draw the last frame)
-  let runTime = round((millis()-startTime)/1000);
   
-  let debugString = "DEBUG/PERFORMANCE INFO\nPosition (X,Y): "+round(x0,3)+", "+round(y0,3)+'\nSpeed:            '+speed; //basic debug info string
-  
-  let performanceString = '\nCurrent FPS:   '+realFPS+"fps\nTarget FPS:     "+refresh+'fps\nRendered:       '+frameCount+' Frames, '+runTime+'Seconds\nCurrent Delta: '+deltaTime+'ms\nTarget Delta:   '+(1/refresh)*1000+'ms\nWhy does tinting anything slow everything down so much?'
+  let debugString = "DEBUG/PERFORMANCE INFO\nPosition (X,Y): "+round(x0,3)+", "+round(y0,3)+'\nSpeed:            '+speed+'\nCurrent FPS:   '+realFPS+"fps\nTarget FPS:     "+refresh+'fps\nRendered:       '+frameCount+' Frames, '+round((millis()-startTime)/1000)+'Seconds\nCurrent Delta: '+deltaTime+'ms\nTarget Delta:   '+(1/refresh)*1000+'ms\nWhy does tinting anything slow everything down so much?'
   
   fill(100)//set color for text
   textSize(20)//set text for general info
   text((infoString),10,30);//print basic info to screen, have to redraw every time since i cant include it in the bg
   textSize(17)//set size for debug info
-  text((debugString+performanceString),10,90);//print changing info to screen, have to redraw every time since it updates in real time
+  text((debugString),10,90);//print changing info to screen, have to redraw every time since it updates in real time
   
 }
