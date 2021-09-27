4fe85debf83afc1bbb2caf21b5a244ba2c37763b
DMessham <lolcato2700@gmail.com>
2021-09-27 16:45:50 -0600

    DVD: tried to fix some of the floating point weirdness
    the only time you should get weird floating point shenanigans should be from the click to change speed

--- DVD/sketch.js
+++ DVD/sketch.js
@@ -53,7 +53,7 @@ function keyPressed(){//randomize background color on keypress
 }
 
 function mouseClicked(){//randomize speed multiplication on click
-  speed=randomGaussian(1.5,1);
+  speed=round(randomGaussian(1.5,1),2);
 }
 
 function draw() {
@@ -67,17 +67,17 @@ function move(){//basic colllision logic
 
   if (x0 + sizeX0 >= width || x0 <= 0){//change direction if the object is within size of the edge of x
     dx0=-dx0;//invert y direction
-    x0 += dx0*speed*round(deltaTime/((1/refresh)*815))
-    tint(random(128,255),random(128,192),random(64,192))//randomly tint the image
+    x0 += dx0*speed*round(deltaTime/((1/refresh)*915),2)
+    tint(round(random(128,255)),round(random(128,192)),round(random(64,192)))//randomly tint the image
   }
-  x0 += dx0*speed*round(deltaTime/((1/refresh)*815));//increase x by dx every time the screen redraws
+  x0 += dx0*speed*round(deltaTime/((1/refresh)*915));//increase x by dx every time the screen redraws
   
   if (y0 + sizeY0 >= height || y0 <= 0){//change direction if the object is within size of the edge of y
     dy0=-dy0;//invert y direction
-    y0 += dy0*speed*round(deltaTime/((1/refresh)*815))
-    tint(random(128,255),random(128,192),random(128,192));//randomly tint the image
+    y0 += dy0*speed*round(deltaTime/((1/refresh)*915),2)
+    tint(round(random(128,255)),round(random(128,192)),round(random(64,192)))//randomly tint the image
   }
-  y0 += dy0*speed*round(deltaTime/((1/refresh)*815));//move in y axis
+  y0 += dy0*speed*round(deltaTime/((1/refresh)*915));//move in y axis
 
   if(x0>width+15 || x0<-15 ||y0>height+15 ||y0<-15){//return to centre if outside bounds
     x0=width/2;
@@ -89,5 +89,5 @@ function txtInfo(){
   textSize(20)//set text for general info
   text("Daniel Messham's JavaScript DVD Logo Bounce\nComp sci 30, S1, Monday September 27th, 2021",10,30);//information string used for author info)
   textSize(17)//set size for debug info
-  text(("DEBUG/PERFORMANCE INFO\nPosition (X,Y): "+round(x0,2)+"/"+windowWidth+", "+round(y0,2)+"/"+windowHeight+'\nSpeed Mult:     '+speed+'\nCurrent FPS:   '+round((refresh/deltaTime)*30,2)+"fps\nTarget FPS:     "+refresh+'fps\nRendered:       '+frameCount+' Frames, '+round((millis()/1-startTime)/1000)+' Seconds\nCurrent Delta: '+round(deltaTime*1,2)+'ms\nTarget Delta:   '+round(((1/refresh)*1000),2)+'ms'),10,90);
+  text(("DEBUG/PERFORMANCE INFO\nPosition (X,Y): "+x0+"/"+windowWidth+", "+y0+"/"+windowHeight+'\nSpeed Mult:     '+speed+'\nCurrent FPS:   '+round((refresh/deltaTime)*30,2)+"fps\nTarget FPS:     "+refresh+'fps\nRendered:       '+frameCount+' Frames, '+round((millis()/1-startTime)/1000)+' Seconds\nCurrent Delta: '+round(deltaTime*1,2)+'ms\nTarget Delta:   '+round(((1/refresh)*1000),2)+'ms'),10,90);
 }
