77ee65699eb3f23daf22768543a0f15a4085b235
DMessham <20892935+DMessham@users.noreply.github.com>
2021-10-19 12:13:28 -0600

    Readded index.html, style.css, improved dungeon crawl

--- DungeonCrawl/sketch.js
+++ DungeonCrawl/sketch.js
@@ -1,4 +1,4 @@
-// Dungeon crawl
+// Conways game of life
 // Daniel Messham
 // Date
 //
@@ -6,152 +6,149 @@
 // - describe what you did to take this project "above and beyond"
 
 
-let refresh = 60//target framerate, used to calc realfps
-let millisecond = 0;//initilaize debug values
+let refresh = 15//target framerate, used to calc realfps
 
-let gridX = 100;
-let gridY = 60;
+let gridX = 30;
+let gridY = gridX;
 
 let grid = []
 
-let mouseGridX = 0
-let mouseGridY = 0
-
-let xSize = 0;
+let xSize = 10;
 let ySize = xSize;
 
-//standard object stats
+let autoPlay = false;
 
-let bgHue=(128);//Backgrond hue
-let bgSat=(128);//Backgrond saturation
-let bgBright=(18);//Background brightness
-
-
-let levelObject = {
-  x:3, //initial position
-  y:3, //initial position
-  texture:3, //inital speed X
-  property:3, //inital speed Y
-  ndx:1, //x collision
-  ndy:1, //Y collision
-  timeMult:0.7, // time based speed multiplyer
-  size:15, //initial size
-  sizeX:15,
-  sizeY:15,
-  index:0,
-};
+let level1;
 
 let player = {
-  x:2, //initial position
-  y:2, //initial position
-  dx:30, //inital speed X
-  dy:30, //inital speed Y
-  ndy:5, //speed modifier X
-  ndy:5, //speed modifier Y
-  sizeX:17.5,
-  sizeY:160, //initial size for x and y(squares)
-  speed:1,//combined speed for horiz & vert
+  x:0, //initial position
+  y:0, //initial position
   score:0,//player 1 score
+  items:[1,0,0,0,0,0,0,0,0],
+  health:20,
 };
 
 
-let enemy = {
-  x:4, //initial position
-  y:4, //initial position
-  dx:60, //inital speed X
-  dy:60, //inital speed X
-  ndx:5, //speed modifier X
-  ndy:5, //speed modifier Y
-  type:100, // type
-  difficulty:'very hard',
-  sizeX:17.5,
-  sizeY:160, //initial size for x and y(squares)
-  speed:1,//combined speed for horiz & vert
-  score:0,//score value
-};
-
-//standard object stats
 
 function preload(){
-  ding = loadSound('keemstar.mp3');
+  level1 = loadJSON("assets/terrain/lvl1.json")//assumes grid size is 30
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   frameRate(refresh);
   background(12,35,12);
-  //noStroke()
-  fill(18)
-  colorMode(HSB);
-  imageMode(CENTER);
-  noSmooth();
-  xSize = width/gridX;
-  ySize = height/gridY;
-  grid = CreateEmpty2dArray(gridX,gridY);
-  ding.playMode('sustain');
+  fill(18);
+  stroke(128);
+  xSize = windowWidth/gridX;
+  ySize = windowHeight/gridY;
+  grid = level1;
+  player.x = 0
+  player.y = 0
 }
 
 function windowResized(){
   createCanvas(windowWidth, windowHeight);
-  noStroke()
   
 }
 
 function draw() {
-  //bg()
-  displayGrid()
-  frameDelta = frameCount//*deltaTime/1000
-
+  background(12,35,12);
+  //nextTurn();
+  playerControl();
+  displayGrid();
 }
 
 function mousePressed(){
-  //xSize = width/gridX;
-  //ySize = height/gridY;
-  //ySize = xSize;
-  
-  mouseGridX = Math.floor(mouseX/xSize)
-  mouseGridY = Math.floor(mouseY/ySize)
+  let mouseGridX = Math.floor(mouseX/xSize);
+  let mouseGridY = Math.floor(mouseY/ySize);
 
-  if(grid[mouseGridX][mouseGridY] === 1){
-    grid[mouseGridX][mouseGridY] = 0
+  if (grid[mouseGridY][mouseGridX] === 0) {
+    grid[mouseGridY][mouseGridX] = 1;
   }
-  else if(grid[mouseGridX][mouseGridY] === 0){
-    grid[mouseGridX][mouseGridY] = 1
+  else if (grid[mouseGridY][mouseGridX] === 1) {
+    grid[mouseGridY][mouseGridX] = 0;
   }
-  ding.play();
-  displayGrid()
 }
 
-function bg(){
-  //background(bgHue, bgSat, bgBright);//redraw the bg to ensure that no trails happen
-  background(12,35,12);
-  // for (let x = 30; x < width-30; x += 30) {
-  //   for (let y = 30; y < height-30; y += 30) {
-  //     let bgEffect = 1+int(dist(x, y, player.x, player.y));
-  //     colorMode(HSB);
-  //     fill(bgHue,bgSat*2,bgBright*2,map(bgEffect,0,128,0.9,0.5));
-  //     rect(x, y, 25);
-  //   }
-  // }
+
+// function nextTurn() {
+//   let newBoard = CreateArray(gridX, gridY, 0);
+
+//   for (let y=0; y<gridY; y++) {
+//     for (let x=0; x<gridX; x++) {
+//       let neighbours = 0;
+
+//       //look at all neighbours and count them
+//       for (let i=-1; i<=1; i++) {
+//         for (let j=-1; j<=1; j++) {
+//           if (y+i>=0 && x+j>=0 && y+i<gridY && x+j<gridX) {
+//             neighbours += grid[y+i][x+j];
+//           }
+//         }
+//       }
+
+//       //don't count yourself
+//       neighbours -= grid[y][x];
+
+//       //apply rules of game
+//       if (grid[y][x] === 1) { //alive
+//         if (neighbours === 2 || neighbours === 3) {
+//           newBoard[y][x] = 1;
+//         }
+//         else {
+//           newBoard[y][x] = 0;
+//         }
+//       }
+
+//       if (grid[y][x] === 0) { //dead
+//         if (neighbours === 3) {
+//           newBoard[y][x] = 1;
+//         }
+//         else {
+//           newBoard[y][x] = 0;
+//         }
+//       }
+//     }
+//   }
+//   grid = newBoard;
+// }
+
+function swap(x,y){
+
+  if(x>=0 && x< gridX && y>=0 && y< grid){
+    if(grid[x][y] === 1){
+      grid[x][y] = 0;
+    }
+    else if(grid[x][y] === 0){
+      grid[x][y] = 1;
+    }
+  }
+  displayGrid();
+  return grid;
 }
 
-function CreateEmpty2dArray(row, col){
+function CreateArray(row, col, value){
+  xSize = windowWidth/gridX;
+  ySize = windowHeight/gridY;
   let grid = [];
   for(let y=0; y<row; y++){
     grid.push([])
     for(let x=0; x<col; x++){
-      grid[y].push(0);
+      grid[y].push(value);
     }
   }
   return grid;
 }
 
-function CreateRandom2dArray(row, col){
+function CreateRandom(row, col){
+  xSize = windowWidth/gridX;
+  ySize = windowHeight/gridY;
   let grid = [];
   for(let y=0; y<row; y++){
     grid.push([])
     for(let x=0; x<col; x++){
-      if(random(10)<=5){
+      if(random(100)<=50){
         grid[y].push(1);
       }
       else{grid[y].push(0);}
@@ -160,19 +157,86 @@ function CreateRandom2dArray(row, col){
   return grid;
 }
 
-function KeyIsPressed(){
-  if(keyCode=83){
-    CreateRandom2dArray(row,col);
-    displayGrid();
+
+function keyPressed(){
+  // if(key==="w"){
+  //   playerMove(player.x,player.y-1);
+  // }
+  // else if(key==="s"){
+  //   playerMove(player.x,player.y+1);
+  // }
+  // else if(key==="a"){
+  //   playerMove(player.x-1,player.y);
+  // }
+  // else if(key==="d"){
+  //   playerMove(player.x+1,player.y);
+  // }
+  
+  if(key==="e"){
+    grid = CreateArray(gridX, gridY, 1);
+  }
+  else if(key==="b"){
+    grid = CreateArray(gridX, gridY, 0);
+  }
+  else if(key==="r"){
+    grid = CreateRandom(gridX,gridY);
+  }
+  else if(key==="k"){
+    saveJSON();
+  }
+  else if(key==="l"){
+    loadJSON()
   }
 }
 
-function displayGrid(){
-  for(let y=0; y<gridY; y++){
-    for(let x=0; x<gridX; x++){
-      if (grid[x][y]===0){fill(255);}
-      else if (grid[x][y]===1){fill(20);}
-      rect(x*xSize, y*ySize, xSize,ySize);
+function playerControl(){
+  if (keyIsDown(UP_ARROW)){playerMove(player.x,player.y-1);}
+  if (keyIsDown(DOWN_ARROW)){playerMove(player.x,player.y+1);}
+  if (keyIsDown(LEFT_ARROW)){playerMove(player.x-1,player.y);}
+  if (keyIsDown(RIGHT_ARROW)){playerMove(player.x+1,player.y);}
+
+  if (keyIsDown('w')){playerMove(player.x,player.y-1);}
+  if (keyIsDown('s')){playerMove(player.x,player.y+1);}
+  if (keyIsDown('a')){playerMove(player.x-1,player.y);}
+  if (keyIsDown('d')){playerMove(player.x+1,player.y);}
+}
+
+function playerMove(newX,newY){
+  if (newX>0||newY>0||newX<gridX||newY<gridY){
+    let oldX = player.x
+    let oldY = player.y
+    if (grid[newY][newX]===0){
+      player.x = newX
+      player.y = newY
+      grid[newY][newX] = 2;
+      grid[oldY][oldX]=0
+    }
+    displayGrid()
+  }
+}
+
+function displayGrid() {
+  for (let y=0; y<gridY; y++) {
+    for (let x=0; x<gridX; x++) {
+      if (grid[y][x] === 0) {fill(20);}//empty
+
+      if (grid[y][x] === 1) {fill(180);}//wall
+
+      if (grid[y][x] === 2) {fill(12,67,12);}//player
+      if (x === player.x&&y === player.y) {fill(12,67,12);}//player
+
+      rect(x*xSize, y*ySize,  xSize, ySize);
     }
   }
+}
+
+
+function saveJSON(){
+  saveJSON(grid, "DMessham-gridJS-save.json");
+}
+
+function loadJSON(){
+  let data = []
+  
+  grid = data
 }
