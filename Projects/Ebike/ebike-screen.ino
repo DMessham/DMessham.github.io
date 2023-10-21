5248d29809ee90ec3290f00545bf93ff23d1fa0b
DMessham <20892935+DMessham@users.noreply.github.com>
2023-10-20 22:46:45 -0600

    more arduino work

--- Projects/Ebike/ebike-screen.ino
+++ Projects/Ebike/ebike-screen.ino
@@ -1,10 +1,10 @@
 #include <LiquidCrystal.h>
 /* Pins */
-const int rs = 6, en = 8, d4 = 2, d5 = 3, d6 = 4, d7 = 5, pedalPulse = 7;
+const int rs = 7, en = 8, d4 = 2, d5 = 3, d6 = 4, d7 = 5, pedalPulse = 6;
 
 /* Variables used for calculations */
 const float gearRatio = 2.2, motorMaxRpm = 470, pulsePerRot = 1, wheelDia = 660.4, wheelRoll = 4.14941557686, batteryMaxVoltage = 24, BatteryMinVoltage = 12, MotorMaxAmps=18.75;
- /* wheelRoll is used for speed calc - the distance the wheel travels between sensor pulses in meters*/
+ /* wheelRoll is used for speed calc - the distance the wheel travels between sensor pulses*/
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
  
@@ -13,32 +13,27 @@ void setup()
   lcd.begin(16, 2);
   
   lcd.setCursor( 0, 0 );
-  lcd.print("--.- kmh  --.--V");
+  lcd.print("--.- kmh");
+  delay(2000);
   lcd.setCursor( 0, 1 );
-  lcd.print("---.-RPM  --.--A");
+  lcd.print("batt: ---%");
+  delay(2000);
 } 
  
 void loop()
 {
-/*TODO read other sensors*/
-float voltage = 22.75
-float motorAmps = 16.25
-float motorRPM = 
-/*read buttons and act on them (might be moved off the arduino)*/
 
-/* TODO: Measure pulses on pin 7 over the last second */
-float pulsePerSec
+/* TODO: Measure pulses on pin 6 over the last second */
+int PulsePerSec
 
-  /* calculate speed from wheel sensor - 3.6 is used to convert from m/s to kmh 
-  1 rotations per second is ~ 4.14941557686m/s or ~ 14.9379 kilometers per hour, 
-  this bike should top out at 16.43mph/7.34487 meters per second/26.4415 kilometers per hour under motor power ignoring any resistance or losses*/
+  /* calculate speed from wheel sensor - 0.6 is used to convert m/s to kmh*/
 
-int speed = pulsePerSec/pulsePerRot*wheelRoll*3.6;
+int speed = PulsePerSec/pulsePerRot*wheelRoll*0.6;
   
   /* Update screen */
   lcd.setCursor( 0, 0 );
-  lcd.print(speed + "KMH, " + voltage + "V");
+  lcd.print(Speed+"kmh");
   lcd.setCursor( 0, 1 );
-  lcd.print(motorRPM + "RPM, " + motorAmps +"A");
+  lcd.print("batt: ---%");
   delay(2000);
 }
